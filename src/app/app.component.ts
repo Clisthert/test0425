6df2838f806acbd401df32142e8f777616cf12bc
Alex <alexsites824@gmail.com>
2023-02-15 12:57:42 -0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,40 @@
-import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   @ViewChild('agGrid') agGrid!: AgGridAngular;
-  
+
   title = 'project-aggrid';
 
   //{headerName: 'Make', field: 'make', sortable: true, filter: true, checkboxSelection: true, rowGroup: true },
 
-    columnDefs = [
-        {headerName: 'Country', field: 'country', sortable: true, filter: true},
-        {headerName: 'Athlete', field: 'athlete', sortable: true, filter: true},
-        {headerName: 'Age', field: 'age', sortable: true, filter: true},
-        {headerName: 'Date', field: 'date', sortable: true, filter: true},
-        {headerName: 'Sport', field: 'sport', sortable: true, filter: true}
-    ];
+  columnDefs = [
+    { headerName: 'Prod', field: 'name', sortable: true, filter: true },
+    {
+      headerName: 'Description',
+      field: 'description',
+      sortable: true,
+      filter: true,
+    },
+    { headerName: 'Price', field: 'price', sortable: true, filter: true },
+    { headerName: 'Quantity', field: 'quantity', sortable: true, filter: true },
+    { headerName: 'Date', field: 'date', sortable: true, filter: true },
+  ];
 
   autoGroupColumnDef = {
-      field: 'Country',
-      suppressMovable: true,
-      headerName: 'Country',
-      cellRenderer: 'agGroupCellRenderer',
-      // cellRendererParams: {
-      //     checkbox: true
-      // }
+    field: 'Prod',
+    suppressMovable: true,
+    headerName: 'Prod',
+    cellRenderer: 'agGroupCellRenderer',
+    // cellRendererParams: {
+    //     checkbox: true
+    // }
   };
 
   rowData: any;
@@ -38,7 +42,10 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-     this.rowData = this.http.get('https://www.ag-grid.com/example-assets/olympic-winners.json');
+    this.rowData = this.http.get(
+      'https://www.bateriasjr.com.br/js/olympic-winners.json'
+    );
+    //this.rowData = this.http.get('https://www.ag-grid.com/example-assets/olympic-winners.json');
     //this.rowData = this.http.get('https://www.ag-grid.com/example-assets/row-data.json');
   }
 
@@ -48,5 +55,4 @@ export class AppComponent {
   //   const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
   //   alert(`Selected nodes: ${selectedDataStringPresentation}`);
   // }
-
 }
